099a6674957afa4d1032369d1de0f0b346d1e688
Aman Pal <89877972+Amanpal222@users.noreply.github.com>
2022-02-02 22:48:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+
+var arr = [2,7,11,15,14,5,9,6];
+var target = 19;
+var out=document.getElementById("output")
+function IndexFind(){
+
+for(let i = 0 ; i < arr.length ; i++){
+    for (let j = i + 1; j <= arr.length - 1; j++){
+        if(target == arr[i] + arr[j]){
+            
+            out.innerHTML=(`The indexes with sum  = 19 is ${[i,j]}`)
+        }
+      
+    }
+}
+}
